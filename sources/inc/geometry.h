62652783463459b19e421ab94a1ef79a8b1e4aab
Ofghanirre <aleronraider@gmail.com>
2024-03-21 11:57:47 +0100

    add geometry operations for vectors

--- sources/inc/geometry.h
+++ sources/inc/geometry.h
@@ -11,6 +11,34 @@ typedef struct _rgb {
 
 Pos3d Pos3d_add(Pos3d a, Pos3d b);
 
+Pos3d Pos3d_add3d(Pos3d a, Pos3d b);
+
 Pos3d Pos3d_mul(Pos3d a, double s);
+
+Pos3d Pos3d_mul3d(Pos3d a, Pos3d b);
+
+Pos3d Pos3d_sub(Pos3d a, Pos3d b);
+
+Pos3d Pos3d_sub3d(Pos3d a, Pos3d b);
+
+Pos3d Pos3d_div(Pos3d a, double s);
+
+Pos3d Pos3d_div3d(Pos3d a, Pos3d b);
+
+double Pos3d_distance(Pos3d a, Pos3d b);
+
+double Pos3d_dot(Pos3d a, Pos3d b);
+
+#define Vec3d_add(a, b) Pos3d_add(a, b)
+#define Vec3d_add3d(a, b) Pos3d_add3d(a, b)
+#define Vec3d_mul(a, s) Pos3d_mul(a, s)
+#define Vec3d_mul3d(a, b) Pos3d_mul3d(a, b)
+#define Vec3d_sub(a, b) Pos3d_sub(a, b)
+#define Vec3d_sub3d(a, b) Pos3d_sub3d(a, b)
+#define Vec3d_div(a, s) Pos3d_div(a, s)
+#define Vec3d_div3d(a, b) Pos3d_div3d(a, b)
+#define Vec3d_distance(a, b) Pos3d_distance(a, b)
+#define Vec3d_dot(a, b) Pos3d_dot(a, b)
+
 
 #endif
--- sources/src/geometry.c
+++ sources/src/geometry.c
@@ -1,6 +1,6 @@
 #include <geometry.h>
 
-Pos3d Pos3d_add(Pos3d a, Pos3d b) {
+static Pos3d Pos3d_add(Pos3d a, Pos3d b) {
     return (Pos3d){
         .x = a.x + b.x,
         .y = a.y + b.y,
@@ -8,10 +8,66 @@ Pos3d Pos3d_add(Pos3d a, Pos3d b) {
     };
 }
 
-Pos3d Pos3d_mul(Pos3d a, double s) {
+static Pos3d Pos3d_add3d(Pos3d a, Pos3d b) {
+    return (Pos3d){
+        .x = a.x + b.x,
+        .y = a.y + b.y,
+        .z = a.z + b.z,
+    };
+}
+
+static Pos3d Pos3d_mul(Pos3d a, double s) {
     return (Pos3d){
         .x = a.x * s,
         .y = a.y * s,
         .z = a.z * s,
     };
+}
+
+static Pos3d Pos3d_mul3d(Pos3d a, Pos3d b) {
+    return (Pos3d){
+        .x = a.x * b.x,
+        .y = a.y * b.y,
+        .z = a.z * b.z,
+    };
+}
+
+static Pos3d Pos3d_sub(Pos3d a, Pos3d b) {
+    return (Pos3d){
+        .x = a.x - b.x,
+        .y = a.y - b.y,
+        .z = a.z - b.z,
+    };
+}
+
+static Pos3d Pos3d_sub3d(Pos3d a, Pos3d b) {
+    return (Pos3d){
+        .x = a.x - b.x,
+        .y = a.y - b.y,
+        .z = a.z - b.z,
+    };
+}
+
+static Pos3d Pos3d_div(Pos3d a, double s) {
+    return (Pos3d){
+        .x = a.x / s,
+        .y = a.y / s,
+        .z = a.z / s,
+    };
+}
+
+static Pos3d Pos3d_div3d(Pos3d a, Pos3d b) {
+    return (Pos3d){
+        .x = a.x / b.x,
+        .y = a.y / b.y,
+        .z = a.z / b.z,
+    };
+}
+
+static double Pos3d_distance(Pos3d a, Pos3d b) {
+    return sqrt(pow(a.x - b.x, 2) + pow(a.y - b.y, 2) + pow(a.z - b.z, 2));
+}
+
+static double Pos3d_dot(Pos3d a, Pos3d b) {
+    return a.x * b.x + a.y * b.y + a.z * b.z;
 }
